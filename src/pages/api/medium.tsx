50877d69eb612db24ef021688574f233420d02ea
yayan <ya2n.wicaksono@gmail.com>
2025-06-11 14:47:03 +0700

    feat: support cache

--- src/pages/api/medium.tsx
+++ src/pages/api/medium.tsx
@@ -1,10 +1,21 @@
 import {NextApiRequest, NextApiResponse} from 'next';
 import Parser from 'rss-parser';
 
+const parser = new Parser();
+const feedCache = {
+    data: "{}",
+    timestamp: 0,
+};
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const parser = new Parser();
     try {
+        const CACHE_TTL = 1000 * 60 * 60; // 1 hour
+        if (feedCache.data && Date.now() - feedCache.timestamp < CACHE_TTL) {
+            return res.status(200).json(JSON.parse(feedCache.data));
+        }
         const feed = await parser.parseURL('https://medium.com/feed/@yayandwicaksono');
+        feedCache.data = JSON.stringify(feed.items);
+        feedCache.timestamp = Date.now();
         res.status(200).json(feed.items);
     } catch (error) {
         console.error('Error fetching feed:', error);

c0f6097b2000c10b09fa0d54ebb8d08bcea7130e
yayan <ya2n.wicaksono@gmail.com>
2025-07-01 12:56:19 +0700

    feat: change background

--- src/components/tag/tag.tsx
+++ src/components/tag/tag.tsx
@@ -1,5 +1,6 @@
 export default function Tag({children}: { children: React.ReactNode }) {
     return (
-        <span className="bg-gray-200 text-gray-800 rounded-full px-3 py-1 text-sm bouncy-text">{children}</span>
+        <span
+            className="bg-gray-200 text-gray-800 rounded-full px-3 py-1 text-sm bouncy-text hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">{children}</span>
     )
 }

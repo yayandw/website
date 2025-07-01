5ec5f048f16ad48a28f634b6512402c52c4b6e98
yayan <ya2n.wicaksono@gmail.com>
2025-07-01 10:40:32 +0700

    feat: add footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div className="flex items-center justify-center bg-gray-200 text-gray-800 p-1">
+            <p>© 2025 Yayan All Right Reserved</p>
+        </div>
+    )
+}

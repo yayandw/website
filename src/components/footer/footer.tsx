12b4909ee7dfe8877a73e9df1fb70e0fe4db4043
yayan <ya2n.wicaksono@gmail.com>
2025-07-01 11:05:28 +0700

    feat: justify size

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div className="flex items-center justify-center bg-gray-200 text-gray-800 p-1">
+        <div className="flex items-center justify-center bg-gray-200 text-gray-800 p-4">
             <p>© 2025 Yayan All Right Reserved</p>
         </div>
     )
